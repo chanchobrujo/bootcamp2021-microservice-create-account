6f6524c8c32a6f320757bd963edaad726991f491
umb.kevsidorov@gmail.com <umb.kevsidorov@gmail.com>
2021-09-19 12:17:11 -0500

    Update service rules

---
+++ src/main/java/com/everisbootcamp/createaccount/Service/RulesService.java
@@ -0,0 +1,12 @@
+package com.everisbootcamp.createaccount.Service;
+
+import com.everisbootcamp.createaccount.Data.Rules;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RulesService {
+    public Rules addRule(String customerType, String profile){
+        return null;
+    }
+}
