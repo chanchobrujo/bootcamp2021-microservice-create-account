3e1f9359dcb2128b76f7057763cb6cc7f149d07e
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-12-30 18:03:46 -0500

    FEAT: Add util class.

---
+++ src/main/java/com/everisbootcamp/createaccount/Common/Utils.java
@@ -0,0 +1,14 @@
+package com.everisbootcamp.createaccount.Common;
+
+import com.everisbootcamp.createaccount.Constant.Enums.YesOrNot;
+
+public class Utils {
+
+    public static Boolean StringEmpty(String value) {
+        return value.length() == 0 || value == null || value.isEmpty();
+    }
+
+    public static String BooleanToString(Boolean value) {
+        return value ? YesOrNot.YES.getValue() : YesOrNot.NO.getValue();
+    }
+}
